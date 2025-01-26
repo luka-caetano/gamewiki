ad7650b6eec3cd275a885aae910b821afb1b1750
luka-caetano <luka_santos@protonmail.com>
2025-01-26 14:12:02 -0300

    init: Projeto criado

---
+++ src/main/java/com/gamewiki/gamewiki/GamewikiApplication.java
@@ -0,0 +1,13 @@
+package com.gamewiki.gamewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamewiki/gamewiki/GamewikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamewiki.gamewiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamewikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
